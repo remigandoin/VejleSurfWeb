d1ad5d748a0c5b56720e89b1906b898423918057
Rémi Gandoin <144945370+remigandoin@users.noreply.github.com>
2026-07-06 15:41:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def get_forecast(lat, lon, model_name, model_code):
         "longitude": lon,
         "hourly": "temperature_2m,wind_speed_10m,wind_gusts_10m,wind_direction_10m",
         "wind_speed_unit": "kmh",
-        "forecast_days": 3,
+        "forecast_hours": 72,
         "timezone": "auto",
         "models": model_code,
     }
